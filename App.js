e75d2c8cbd3609e77e55c83877d1cf68e29734a7
saurabh <saurabhverma2693@gmail.com>
2025-03-07 17:48:22 +0530

    send response

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ const http = require('http');
  const server = http.createServer((req,res)=>{
 console.log(req.url,req.headers,req.method,req);
 // process.exit();
+res.setHeader('Content-Type','text/html');
+res.write('<html>');
+res.write('<head><tittle>My First Page</tittle></head>');
+res.write('<body><h>Hello from my node.js server....</h></body>');
+res.write('</html>');
+res.end();
 
 });
 server.listen(3000);
