23759d1e8a2e52e3038ac84ba934e321bc7f6055
saurabh <saurabhverma2693@gmail.com>
2025-03-08 14:13:20 +0530

    NPM Scripts

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const routes = require('./routes');
-console.log(routes.smmeText);
+console.log(routes.sometext);
  const server = http.createServer(routes.handler);
 server.listen(3000);
