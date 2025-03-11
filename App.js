6aa48b685820ed3cc78a351b62b173b5fe636c9b
saurabh <saurabhverma2693@gmail.com>
2025-03-11 15:20:19 +0530

    Parse Incoming Request

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 
 const express = require('express');
+
+const bodyParser = require('body-parser');
  const app =  express()
 
 //  app.use((req,res,next)=>{
@@ -12,18 +14,32 @@ app.use((req,res,next)=>{
     next();
 });
 
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.use('/add-product',(req,res,next)=>{
-    console.log('In the anrsother middleware')   
-    res.send('<h1>The add page product</h1>') 
+      console.log("In another middleware");
+    res.send('<form acttion="/product" method="POST"> <input type="text" name="title"> <button type="Submit">Add Product</button> </form>'); 
 });
 
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)   
+    res.redirect('/'); 
+});
 
 app.use('/',(req,res,next)=>{
-    console.log('In the anrsother middleware')   
-    res.send('<h1>Hello from express js</h1>') 
+    console.log('in another middleware')   
+    res.send('<h1>Heelo from Express</h1>'); 
 });
 
-app.listen(3000);
+// app.use('/',(req,res,next)=>{
+//     console.log('In the anrsother middleware')   
+//     res.send('<h1>Hello from express js</h1>') 
+// });
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT,()=>{
+    console.log("server running on port" + PORT);
+});
 
 //  const server = http.createServer(app);
 // server.listen(3000);
