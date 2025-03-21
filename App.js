4cb4b7098e01bde704346f548933fe25a9bbee5c
saurabh <saurabhverma2693@gmail.com>
2025-03-21 12:16:38 +0530

    Adding a 404 Error Page

--- App.js
+++ App.js
@@ -25,6 +25,9 @@ app.use(adminRoutes);
 
 app.use(shopRoutes);
 
+app.use((req,res,next)=>{
+    res.status(404).send('<h1> Page not found</h1>');
+});
 
 
 // app.use('/',(req,res,next)=>{
--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/add-product',(req,res,next)=>{
     console.log("In another middleware");
-  res.send('<form acttion="/product" method="POST"> <input type="text" name="title"> <button type="Submit">Add Product</button> </form>'); 
+  res.setHeader().send('<form acttion="/product" method="POST"> <input type="text" name="title"> <button type="Submit">Add Product</button> </form>'); 
 });
 
 router.post('/product',(req,res,next)=>{
